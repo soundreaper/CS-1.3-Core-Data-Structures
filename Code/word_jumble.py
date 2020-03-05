2290b1ad5703210634beeb8998fac2e533074996
soundreaper@gmail.com <soundreaper@gmail.com>
2020-03-04 16:11:08 -0800

    add docstrings to explain code

--- Code/word_jumble.py
+++ Code/word_jumble.py
@@ -9,7 +9,10 @@ The below code would load and return a list of the built-in Mac dictionary.
 #     d_open.close()
 #     d_list = d_raw.split("\n")
 #     return d_list
-    
+
+'''
+The method below returns a 26-dimension vector that counts the frequency of each letter in a given string.
+'''    
 def wordToDictionary(word):
     letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     vector = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -21,6 +24,9 @@ def wordToDictionary(word):
             vector[ind] += 1
     return vector
 
+'''
+The method below converts the vector from the above function into a 4-bit integer.
+'''
 def vectorToInt(vector):
     power = 0
     num = 0
@@ -31,8 +37,8 @@ def vectorToInt(vector):
     return num
     
 '''
-The below code was used to generate the dict.txt file in the same directory which is a dictionary containing
-integer keys with the words from the dictionary as values. Since the dictionary has already been generated
+The below code was used to generate the dict.txt file in the same directory which contains integer
+keys with the words from the dictionary as values. Since the dictionary has already been generated
 we don't need the below code to be active unless we need to re-generate the dictionary.
 '''    
 # def intToDict(dict):
@@ -54,9 +60,16 @@ we don't need the below code to be active unless we need to re-generate the dict
 # g.write(str(ind))
 # g.close()
 
+'''
+We open the generated dictionary below and read it in as a python dictionary.
+'''
 generatedDictionary = open("dict.txt", "r")
 ind = eval(generatedDictionary.read())
 
+'''
+We take scrambled words as command line arguments and then convert them to integers and attempt to
+match them to the dictionary above.
+'''
 for x in sys.argv[1:]:
     v = vectorToInt(wordToDictionary(str(x)))
     unscrambled = ind.get(v, 'Word Not in Dictionary.')
